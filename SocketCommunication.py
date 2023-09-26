96e02d466f4023d06d55fdda70be13f0908e0d67
yangyi <yangyi@163.com>
2023-09-26 19:28:03 +0800

    两节点链接，实现消息通讯

--- SocketCommunication.py
+++ SocketCommunication.py
@@ -10,6 +10,11 @@ class SocketCommunication(Node):
 
     def inbound_node_connected(self, connected_node):
         print('inbound connection')
+        self.send_to_node(connected_node, '你好，我是你链接的节点')
     
     def outbound_node_connected(self, connected_node):
         print('outbound connection')
+        self.send_to_node(connected_node, '你好，我是要链接你的节点')
+
+    def node_message(self, connected_node, message):
+        print(message)
