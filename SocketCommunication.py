7deb50a9069e2cce4efabd81d6114ffa39ad7aaf
yangyi <yangyi@163.com>
2023-09-26 18:46:50 +0800

    socket类实现

---
+++ SocketCommunication.py
@@ -0,0 +1,9 @@
+from p2pnetwork.node import Node
+
+
+class SocketCommunication(Node):
+    def __init__(self, ip, port):
+        super(SocketCommunication, self).__init__(ip, port, None)
+    
+    def startSocketCommunication(self):
+        self.start()
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from Node import Node
+import sys
 
 
 if __name__ == '__main__':
-    node = Node()
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
 
-    print(node.blockchain)
-    print(node.transactionPool)
-    print(node.wallet)
+    node = Node(ip, port)
+    node.startP2P()  # python main.py localhost 5000
