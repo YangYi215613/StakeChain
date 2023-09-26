25583ac1e4f14742e481447813baedbae03f3709
yangyi <yangyi@163.com>
2023-09-26 14:41:22 +0800

    生成AccountModel类

--- main.py
+++ main.py
@@ -5,37 +5,13 @@ from Block import Block
 from pprint import pprint
 from Blockchain import Blockchain
 from BlockchainUtils import BlockchainUtils
+from AccountModel import AccountModel
+
 
 if __name__ == '__main__':
-    sender = 'sender'
-    receiver = 'receiver'
-    amount = 1
-    type = 'TRANSFER'
-
     wallet = Wallet()
-    fraudulentWallet = Wallet()
-    pool = TransactionPool()
+    accountModel = AccountModel()
+    accountModel.updateBalance(wallet.publicKeyString(), 10)
+    accountModel.updateBalance(wallet.publicKeyString(), -5)
 
-    transaction = wallet.createTransaction(receiver, amount, type)
-
-    if pool.transactionExists(transaction) == False:
-        pool.addTransaction(transaction)
-
-
-    blockchain = Blockchain()
-
-    lastHash = BlockchainUtils.hash(blockchain.blocks[-1].payload()).hexdigest()
-    blockCount = blockchain.blocks[-1].blockCount + 1
-    
-    block = wallet.createBlock(pool.transactions, lastHash, blockCount)
-
-    if not blockchain.lastBlockHashValid(block):
-        print('区块中不是有效的前区块哈希')
-    
-    if not blockchain.blockCountValid(block):
-        print('区块中区块计数不有效')
-
-    if blockchain.lastBlockHashValid(block) and blockchain.blockCountValid(block):
-        blockchain.addBlock(block)
-
-    pprint(blockchain.toJson())
+    print(accountModel.balances)
