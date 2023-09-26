a93d0cd563dd96c2441da1128a9cee669d592b26
yangyi <yangyi@163.com>
2023-09-26 19:44:11 +0800

    工具类构建

---
+++ Message.py
@@ -0,0 +1,6 @@
+class Mesage:
+
+    def __init__(self, senderConnector, messageType, data):
+        self.senderConnector = senderConnector
+        self.messageType = messageType
+        self.data = data
