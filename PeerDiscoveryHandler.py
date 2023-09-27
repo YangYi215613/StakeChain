1aa562644d9efda7738b70be3767197ad474d776
yangyi <yangyi@163.com>
2023-09-27 09:28:40 +0800

    节点发现算法启动

---
+++ PeerDiscoveryHandler.py
@@ -0,0 +1,22 @@
+import threading
+import time
+
+class PeerDiscoveryHandler:
+    def __init__(self, node):
+        self.SocketCommunication = node
+
+    def status(self):
+        while True:
+            print('status')
+            time.sleep(10) 
+    
+    def dicovery(self):
+        while True:
+            print('discovery')
+            time.sleep(10)
+
+    def start(self):
+        statusThread = threading.Thread(target=self.status, args=())
+        statusThread.start()
+        discoveryThread = threading.Thread(target=self.dicovery, args=())
+        discoveryThread.start()
--- SocketCommunication.py
+++ SocketCommunication.py
@@ -1,12 +1,16 @@
 from p2pnetwork.node import Node
+from PeerDiscoveryHandler import PeerDiscoveryHandler
 
 
 class SocketCommunication(Node):
     def __init__(self, ip, port):
         super(SocketCommunication, self).__init__(ip, port, None)
+        self.peers = []  
+        self.peerDiscoveryHandler = PeerDiscoveryHandler(self)
     
     def startSocketCommunication(self):
-        self.start()
+        self.start()  # 启动socket线程
+        self.peerDiscoveryHandler.start()  # 启动节点发现
 
     def inbound_node_connected(self, connected_node):
         print('inbound connection')
