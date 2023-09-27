39c36ca6f89917dcfd56d90e905a1536c15ecb7c
yangyi <yangyi@163.com>
2023-09-27 14:45:09 +0800

    pos类中方法实现

--- ProofOfStake.py
+++ ProofOfStake.py
@@ -1,3 +1,6 @@
+from Lot import Lot
+from BlockchainUtils import BlockchainUtils
+
 class ProofOfStake:
     def __init__(self):
         """
@@ -17,3 +20,29 @@ class ProofOfStake:
             return self.stakes[publicKeyString]
         else:
             return None
+
+    def validatorLots(self, seed):
+        lots = []
+        for validator in self.stakes.keys():
+            for stake in range(self.get(validator)):
+                lots.append(Lot(validator, stake+1, seed))
+        
+        return lots
+
+    def winnerLot(self, lots, seed):
+        winnerLot = None
+        leastOffSet = None
+        referenceHashIntValue = int(BlockchainUtils.hash(seed).hexdigest(), 16)
+
+        for lot in lots:
+            lotIntValue = int(lot.lotHash(), 16)
+            offset = abs(lotIntValue - referenceHashIntValue)
+            if leastOffSet is None or offset < leastOffSet:
+                leastOffSet = offset
+                winnerLot = lot
+        return winnerLot
+    
+    def forger(self, lastBlockHash):
+        lots = self.validatorLots(lastBlockHash)
+        winnerLot = self.winnerLot(lots, lastBlockHash)
+        return winnerLot.publicKey
