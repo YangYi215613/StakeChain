32c08270c12f29bb3226f51c1dd3879bf622d58f
yangyi <yangyi@163.com>
2023-09-27 09:58:22 +0800

    HandShake完成

--- SocketCommunication.py
+++ SocketCommunication.py
@@ -1,5 +1,6 @@
 from p2pnetwork.node import Node
 from PeerDiscoveryHandler import PeerDiscoveryHandler
+from SocketConnector import SocketConnector
 
 
 class SocketCommunication(Node):
@@ -7,10 +8,16 @@ class SocketCommunication(Node):
         super(SocketCommunication, self).__init__(ip, port, None)
         self.peers = []  
         self.peerDiscoveryHandler = PeerDiscoveryHandler(self)
+        self.socketConnector = SocketConnector(ip, port)
     
+    def connectFirstNode(self):
+        if not self.socketConnector.port == 10001:
+            self.connect_with_node('localhost', 10001)
+
     def startSocketCommunication(self):
         self.start()  # 启动socket线程
         self.peerDiscoveryHandler.start()  # 启动节点发现
+        self.connectFirstNode()
 
     def inbound_node_connected(self, connected_node):
         self.peerDiscoveryHandler.handshake(connected_node)
