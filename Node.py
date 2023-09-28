f4c9fb396fbddceede03b68f0e032fe468c69459
yangyi <yangyi@163.com>
2023-09-28 15:36:01 +0800

    新节点或丢失数据节点进行数据同步

--- Node.py
+++ Node.py
@@ -5,6 +5,7 @@ from SocketCommunication import SocketCommunication
 from NodeAPI import NodeAPI
 from Message import Message
 from BlockchainUtils import BlockchainUtils
+import copy
 
 
 class Node:
@@ -58,6 +59,11 @@ class Node:
         forgerValid = self.blockchain.forgerValid(block)  # 验证铸造者是否有效
         transactionValid = self.blockchain.transactionValid(block.transactions)  # 验证区块中交易是否有效
         signatureValid = Wallet.signatureValid(blockHash, signature, forger)  # 验证区块签名是否有效
+
+        # 如果区块数目无效，向其余节点发送请求数据信息
+        if not blockCountValid:
+            self.requesetChain()
+
         if lastBlockHashValid and forgerValid and transactionValid and signatureValid and blockCountValid:
             # 该节点打包交易
             self.blockchain.addBlock(block)
@@ -67,6 +73,28 @@ class Node:
             encodedMessage = BlockchainUtils.encode(message)
             self.p2p.broadcast(encodedMessage)
 
+    def requesetChain(self):
+        """广播请求区块链数据消息"""
+        message = Message(self.p2p.socketConnector, 'BLOCKCHAINREQUEST', None)
+        encodedMessage = BlockchainUtils.encode(message)
+        self.p2p.broadcast(encodedMessage)
+
+    def handleBlockchainRequest(self, requestingNode):
+        message = Message(self.p2p.socketConnector, 'BLOCKCHAIN', self.blockchain)
+        encodedMessage = BlockchainUtils.encode(message)
+        self.p2p.send(requestingNode, encodedMessage)
+
+    def handleBlockchain(self, blockchain):
+        """进行增量数据同步"""
+        localBlockchainCopy = copy.deepcopy(self.blockchain)
+        localBlockCount = len(localBlockchainCopy.blocks)
+        receivedChainBlockCount = len(blockchain.blocks)
+        if localBlockCount < receivedChainBlockCount:
+            for blockNumber, block in enumerate(blockchain.blocks):
+                if blockNumber >= localBlockCount:
+                    localBlockchainCopy.addBlock(block)
+                    self.transactionPool.removeFromPool(block.transactions)
+            self.blockchain = localBlockchainCopy
 
     def forge(self):
         forger = self.blockchain.nextForger()
